26ad7aac3bca30e44e97d6787a2ff7ca5f76e7e9
Kbroo <kbroomyk@gmail.com>
2025-12-07 10:33:28 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
 
-    setlocale(LC_ALL, "Russian");
-
+    cout << "Laboratory work #8. Git" << endl;
     cout << "=== Vatiant 6: Roza Vetrov ===" << endl;
     cout << "Student: Kvit Ilya" << endl;
     cout << "Group: 25Pinzh1d" << endl;
