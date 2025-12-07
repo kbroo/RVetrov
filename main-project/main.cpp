68fe72a2a7f12775ddf2b172582360efa5bb0dfd
kbroo <160418774+kbroo@users.noreply.github.com>
2025-12-07 09:46:23 +0300

    Update main.cpp

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,9 +7,9 @@ int main()
 
     setlocale(LC_ALL, "Russian");
 
-    cout << "=== Вариант 6: Роки ветров ===" << endl;
-    cout << "Студент: Квит Илья" << endl;
-    cout << "Группа: 25Пинж1д" << endl;
+    cout << "=== Vatiant 6: Roza Vetrov ===" << endl;
+    cout << "Student: Kvit Ilya" << endl;
+    cout << "Group: 25Pinzh1d" << endl;
 
     return 0;
 
