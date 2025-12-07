b5b98d702ec61a0ea80d653341faf80d289c413f
kbroo <160418774+kbroo@users.noreply.github.com>
2025-12-07 09:43:15 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main()
 {
 
-    cout << "Hello\n";
+    setlocale(LC_ALL, "Russian");
+
+    cout << "=== Вариант 6: Роки ветров ===" << endl;
+    cout << "Студент: Квит Илья" << endl;
+    cout << "Группа: 25Пинж1д" << endl;
 
     return 0;
 
